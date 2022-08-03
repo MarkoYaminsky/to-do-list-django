822e2599d5f2b55eb7d81233332a280b2633f26e
Marko Yaminsky <marko.yaminskiy03@gmail.com>
2022-08-03 17:40:22 +0300

    added user login

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import authenticate
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -23,3 +25,14 @@ class RegistrationAPIView(CreateAPIView):
             "token": User.objects.get(username=data['username']).token().key
         }, status=status.HTTP_201_CREATED)
 
+
+class LoginAPIView(CreateAPIView):
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        user = authenticate(username=data['username'], password=data['password'])
+
+        if not user:
+            return Response("404 USER NOT FOUND", status=status.HTTP_404_NOT_FOUND)
+
+        token = Token.objects.get(user=user).key
+        return Response(token, status=status.HTTP_200_OK)
