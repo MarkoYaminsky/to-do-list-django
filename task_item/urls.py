21cf24bc1627baa46269bd9742a21035adb2ef4e
Marko Yaminsky <marko.yaminskiy03@gmail.com>
2022-08-06 00:32:18 +0300

    added more safety to deleting and patching

--- task_item/urls.py
+++ task_item/urls.py
@@ -4,6 +4,6 @@ from task_item import views
 
 urlpatterns = [
     path('taskitems', views.TaskItemListCreateAPIView.as_view()),
-    path('taskitems/<id>/delete', views.TaskItemDeleteAPIView.as_view()),
+    path('taskitems/<pk>/delete', views.TaskItemDeleteAPIView.as_view()),
     path('taskitems/<pk>/patch', views.TaskItemPatchAPIView.as_view())
 ]
--- task_item/views.py
+++ task_item/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, status
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -10,7 +11,7 @@ from .serializers import TaskItemPatchSerializer
 from .models import TaskItem
 
 
-class TaskItemListCreateAPIView(generics.ListCreateAPIView):
+class TaskItemListCreateAPIView(ListCreateAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_classes = {'create': TaskItemCreateSerializer, 'list': TaskItemListSerializer}
 
@@ -35,19 +36,38 @@ class TaskItemListCreateAPIView(generics.ListCreateAPIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class TaskItemDeleteAPIView(generics.DestroyAPIView):
+class TaskItemDeleteAPIView(APIView):
     permission_classes = (IsAuthenticated,)
     queryset = TaskItem.objects.all()
-    lookup_field = 'id'
+
+    def delete(self, request, pk):
+        user = request.user
+
+        try:
+            task = self.queryset.filter(user=user).get(id=pk)
+        except TaskItem.DoesNotExist:
+            return Response({"detail": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        task.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class TaskItemPatchAPIView(APIView):
     serializer = TaskItemPatchSerializer
     permission_classes = (IsAuthenticated,)
+    queryset = TaskItem.objects.all()
 
     def patch(self, request, pk):
         data = request.data
+        user = request.user
         serializer = self.serializer()
-        task = TaskItem.objects.get(id=pk)
+
+        try:
+            task = self.queryset.filter(user=user).get(id=pk)
+        except TaskItem.DoesNotExist:
+            return Response({"detail": "Task not found"}, status=status.HTTP_404_NOT_FOUND)
+
         serializer.update(task, data)
-        return Response(status=status.HTTP_200_OK)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
