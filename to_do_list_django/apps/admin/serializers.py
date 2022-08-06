6b8d4e40df4ec1962ad55884345bde47c7ce712d
Marko Yaminsky <marko.yaminskiy03@gmail.com>
2022-08-06 13:03:20 +0300

    added admin serializers, user list and admin create views

---
+++ to_do_list_django/apps/admin/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from ..authentication.models import User
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'is_staff')
+
+
+class AdminUserRegistrationSerializer(serializers.ModelSerializer):
+    SECRET_KEY = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ('SECRET_KEY', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_superuser(username=validated_data['username'], password=validated_data['password'])
--- to_do_list_django/apps/admin/views.py
+++ to_do_list_django/apps/admin/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import status, generics
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.response import Response
 
-# Create your views here.
+from . import serializers
+from ..authentication.models import User
+from ...settings import SECRET_KEY
+
+
+class AdminUserCreateAPIView(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = serializers.AdminUserRegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+
+        if data['SECRET_KEY'] != SECRET_KEY:
+            return Response({"detail": "Method POST forbidden"}, status=status.HTTP_403_FORBIDDEN)
+
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class UserListAPIView(generics.ListAPIView):
+    permission_classes = (IsAdminUser,)
+    serializer_class = serializers.UserListSerializer
+    queryset = User.objects.all()
